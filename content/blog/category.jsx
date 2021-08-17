19be163b273374e56f8267d942ef8f365c4ebf2f
Kumi Takenaka <kumireliy@gmail.com>
2021-08-17 16:18:21 +0900

    added widget

---
+++ content/blog/category.jsx
@@ -0,0 +1,28 @@
+import preact from 'preact';
+
+const Category = () => {
+  const title = "TEST CMS!!"
+  const date = ' 2019-09-07T05:43:53.681Z'
+  const tags = 'test, cms'
+  return (
+      <div></div>
+  )
+}
+
+export default Category
+
+// import { h } from 'preact';
+// import { Link } from 'preact-router/match';
+// import style from './style';
+
+// const Header = () => (
+// 	<header class={style.header}>
+// 		<Link href="/"><h1>Jane Doe</h1></Link>
+// 		<nav>
+// 			<Link activeClassName={style.active} href="/blogs">Blogs</Link>
+// 			<Link activeClassName={style.active} href="/contact">Contact me</Link>
+// 		</nav>
+// 	</header>
+// );
+
+// export default Header;
